6b07c3769eae05315e5ae62e13858f5df6ad70f6
Jairo310 <jairo.ps.gdm@gmail.com>
2021-10-08 19:53:42 +0200

    Columnas iniciales y puntuacion
    Se generan columnas en el espacio vacio del principio para que no se quede inutilizado y un mini contador de segundos que pasan para saber cuando tiempo llevas vivo.

--- PR_ZAXXON_PinaJairo/Assets/Scripts/Instanciador.cs
+++ PR_ZAXXON_PinaJairo/Assets/Scripts/Instanciador.cs
@@ -15,6 +15,8 @@ public class Instanciador : MonoBehaviour
     int speed;
     int puntuacion;
 
+    //Variables para instanciar columnas iniciales
+    [SerializeField] float posZcolumna1 = 50f;
 
 
     // Start is called before the first frame update
@@ -24,9 +26,22 @@ public class Instanciador : MonoBehaviour
         variables_Publicas = otroObjeto.GetComponent<Variables_Publicas>();
         // intervalo = 0.3f;
         distance = 10f;
-       
-        
         StartCoroutine("CrearColumna");
+        //Creacion de columnas al inicion del juego
+        float posZ = transform.position.z;
+        float columnasIniciales = (posZ - posZcolumna1) / distance;
+
+        columnasIniciales = Mathf.Round(columnasIniciales);
+        print(columnasIniciales);
+        for(float n= posZcolumna1; n < posZ; n += distance)
+        {
+            float randomX = Random.Range(-18f, 18f);
+            float randomY = Random.Range(0.5f, 10.5f);
+            Vector3 columnaInicialPos = new Vector3(randomX,randomY, n);
+            Instantiate(obstaculos[0],columnaInicialPos,Quaternion.identity);
+        }
+        
+        
     }
 
     // Update is called once per frame
@@ -42,7 +57,6 @@ public class Instanciador : MonoBehaviour
 
             speed = variables_Publicas.speed;
             intervalo = distance / speed;
-            print(intervalo);
            //Generacion aleatoria de prefabs
             float randomX = Random.Range(-18f, 18f);
             float randomY = Random.Range(0.5f, 10.5f);
@@ -55,6 +69,7 @@ public class Instanciador : MonoBehaviour
         }
     }
 
+   
         
     }
 
--- PR_ZAXXON_PinaJairo/Assets/Scripts/Variables_Publicas.cs
+++ PR_ZAXXON_PinaJairo/Assets/Scripts/Variables_Publicas.cs
@@ -7,17 +7,28 @@ public class Variables_Publicas : MonoBehaviour
     public int speed;
     public int puntuacion;
     public int vidas;
-   
+
 
     // Start is called before the first frame update
     void Start()
     {
         speed = 25;
+        StartCoroutine("puntSum");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    IEnumerator puntSum()
+    {
+        while (true)
+        {
+            puntuacion++;
+            print(puntuacion);
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
