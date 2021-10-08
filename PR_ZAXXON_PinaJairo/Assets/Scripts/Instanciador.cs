dc30c9fd14c16f3831d734b22888b99c592fb108
Jairo310 <jairo.ps.gdm@gmail.com>
2021-10-08 19:06:55 +0200

    Segundo obstaculo y instanciado de objetos
    creacion de un segundo obstaculo e instaciado de los obstaculos entorno a la velocidad y el espacio entre los mismos.

--- PR_ZAXXON_PinaJairo/Assets/Scripts/Instanciador.cs
+++ PR_ZAXXON_PinaJairo/Assets/Scripts/Instanciador.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class Instanciador : MonoBehaviour
 {
-    float intervalo;
-    [SerializeField] GameObject columna;
+    public float intervalo;
+    [SerializeField] float distance;
+    [SerializeField] GameObject [] obstaculos;
     [SerializeField] Transform instantiatePosicion;
+
+
+    [SerializeField] GameObject otroObjeto;
+    private Variables_Publicas variables_Publicas;
+    int speed;
+    int puntuacion;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        intervalo = 0.5f;
+        otroObjeto = GameObject.Find("Variables");
+        variables_Publicas = otroObjeto.GetComponent<Variables_Publicas>();
+        // intervalo = 0.3f;
+        distance = 10f;
+       
+        
         StartCoroutine("CrearColumna");
     }
 
@@ -27,13 +39,23 @@ public class Instanciador : MonoBehaviour
     {
         while (true)
         {
-            //Generacion aleatoria de prefabs
+
+            speed = variables_Publicas.speed;
+            intervalo = distance / speed;
+            print(intervalo);
+           //Generacion aleatoria de prefabs
             float randomX = Random.Range(-18f, 18f);
-            Vector3 newPos = new Vector3(randomX, instantiatePosicion.position.y, instantiatePosicion.position.z);
-            Instantiate(columna, newPos, Quaternion.identity);
+            float randomY = Random.Range(0.5f, 10.5f);
+            Vector3 newPos = new Vector3(randomX, randomY, instantiatePosicion.position.z);
+            //Genero un numero aleatorio para elegir objeto
+            int numA = Random.Range(0, obstaculos.Length);
+            Instantiate(obstaculos[numA], newPos, Quaternion.identity);
 
             yield return new WaitForSeconds(intervalo);
         }
     }
 
-}
+        
+    }
+
+
