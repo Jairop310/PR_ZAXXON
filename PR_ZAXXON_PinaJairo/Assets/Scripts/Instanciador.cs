5c1e60b82d9ae15efeb6bd1552bb9a8a2c612899
Jairo Piña Salas <jairo.ps.gdm@gmail.com>
2021-10-12 18:36:33 +0200

    Creacion de varios obstaculos y diferente posición de instanciación.

--- PR_ZAXXON_PinaJairo/Assets/Scripts/Instanciador.cs
+++ PR_ZAXXON_PinaJairo/Assets/Scripts/Instanciador.cs
@@ -24,24 +24,10 @@ public class Instanciador : MonoBehaviour
     {
         otroObjeto = GameObject.Find("Variables");
         variables_Publicas = otroObjeto.GetComponent<Variables_Publicas>();
-        // intervalo = 0.3f;
         distance = 10f;
-        StartCoroutine("CrearColumna");
-        //Creacion de columnas al inicion del juego
-        float posZ = transform.position.z;
-        float columnasIniciales = (posZ - posZcolumna1) / distance;
 
-        columnasIniciales = Mathf.Round(columnasIniciales);
-        print(columnasIniciales);
-        for(float n= posZcolumna1; n < posZ; n += distance)
-        {
-            float randomX = Random.Range(-18f, 18f);
-            float randomY = Random.Range(0.5f, 10.5f);
-            Vector3 columnaInicialPos = new Vector3(randomX,randomY, n);
-            Instantiate(obstaculos[0],columnaInicialPos,Quaternion.identity);
-        }
-        
-        
+        StartCoroutine("CrearColumna");
+        ColumnasIniciales();
     }
 
     // Update is called once per frame
@@ -50,22 +36,62 @@ public class Instanciador : MonoBehaviour
         
     }
 
+    //Creacion de Columnas infinitas limitando el espacio de creacion dependiendo de la "Tag"
     IEnumerator CrearColumna()
     {
         while (true)
         {
-
-            speed = variables_Publicas.speed;
-            intervalo = distance / speed;
-           //Generacion aleatoria de prefabs
-            float randomX = Random.Range(-18f, 18f);
-            float randomY = Random.Range(0.5f, 10.5f);
-            Vector3 newPos = new Vector3(randomX, randomY, instantiatePosicion.position.z);
             //Genero un numero aleatorio para elegir objeto
             int numA = Random.Range(0, obstaculos.Length);
+            float randomX;
+            float randomY;
+            speed = variables_Publicas.speed;
+            intervalo = distance / speed;
+           //Posicionamiento de los prefabs aleatorio y dependiendo del tipo de prefab
+           if(obstaculos[numA].tag == "Pared")
+            {
+                randomX = Random.Range(-18f, 18f);
+                randomY = 0f;
+            }
+           else if (obstaculos[numA].tag == "Pared Superior")
+            {
+                randomX = Random.Range(-18f, 18f);
+                randomY = 9.5f;
+            }   
+           else if(obstaculos[numA].tag== "Pared Con Hueco")
+            {
+                randomY = 0f;
+                randomX = 0f;
+            }
+            else 
+            {
+                randomX = Random.Range(-18f, 18f);
+                randomY = Random.Range(0.5f, 10.5f);
+            }
+            
+            Vector3 newPos = new Vector3(randomX, randomY, instantiatePosicion.position.z);
+            
             Instantiate(obstaculos[numA], newPos, Quaternion.identity);
 
             yield return new WaitForSeconds(intervalo);
+        }
+    }
+
+    //Creacion de columnas al inicion del juego
+    void ColumnasIniciales()
+    {
+        
+        float posZ = transform.position.z;
+        float columnasIniciales = (posZ - posZcolumna1) / distance;
+
+        columnasIniciales = Mathf.Round(columnasIniciales);
+        print(columnasIniciales);
+        for (float n = posZcolumna1; n < posZ; n += distance)
+        {
+            float randomX = Random.Range(-18f, 18f);
+            float randomY = Random.Range(0.5f, 10.5f);
+            Vector3 columnaInicialPos = new Vector3(randomX, randomY, n);
+            Instantiate(obstaculos[0], columnaInicialPos, Quaternion.identity);
         }
     }
 
--- PR_ZAXXON_PinaJairo/Assets/Scripts/Variables_Publicas.cs
+++ PR_ZAXXON_PinaJairo/Assets/Scripts/Variables_Publicas.cs
@@ -9,6 +9,7 @@ public class Variables_Publicas : MonoBehaviour
     public int vidas;
 
 
+
     // Start is called before the first frame update
     void Start()
     {
